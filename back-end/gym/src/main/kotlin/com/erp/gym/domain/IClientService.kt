5d3f4990f391fec19c7bd66af705bb4396e2cd71
Anastasios Fortis <anastasiosfortis@gmail.com>
2019-10-26 19:29:17 +0300

    * Save

--- back-end/gym/src/main/kotlin/com/erp/gym/domain/IClientService.kt
+++ back-end/gym/src/main/kotlin/com/erp/gym/domain/IClientService.kt
@@ -3,6 +3,6 @@ package com.erp.gym.domain
 interface IClientService {
     fun findId(id:Int):Iterable<Client>
     fun findAll():Iterable<Client>
-    fun findUser(username:String,surname:String):Iterable<Client>
+    fun findUser(name:String,surname:String):Iterable<Client>
 
 }
